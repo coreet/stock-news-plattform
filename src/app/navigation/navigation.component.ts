95deed50d89fe81077bfc4283c34e6c74211b3d3
Tiago Santos <80895467+coreet@users.noreply.github.com>
2024-02-29 23:05:32 +0100

    new nav and news-tile

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -4,6 +4,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navigation',
@@ -17,13 +24,39 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
+  animations: [
+    trigger('menuAnimation', [
+      state(
+        'collapsed',
+        style({
+          height: '0',
+          display: 'none',
+        })
+      ),
+      state(
+        'expanded',
+        style({
+          height: '*',
+          display: 'block',
+        })
+      ),
+      transition('collapsed => expanded', [animate('0.3s ease-out')]),
+      transition('expanded => collapsed', [animate('0.3s ease-in')]),
+    ]),
+  ],
 })
 export class NavigationComponent {
   @Output() navToggle = new EventEmitter();
+  public menuOpen: boolean = false;
   constructor() {}
 
   onToggleSidenav(type: NavTypes) {
     this.navToggle.emit(type);
+    this.menuOpen = !this.menuOpen;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 }
 
--- src/app/news-tile/news-tile.component.ts
+++ src/app/news-tile/news-tile.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-news-tile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './news-tile.component.html',
   styleUrls: ['./news-tile.component.scss'],
 })
 export class NewsTileComponent {
   @Input() news: any;
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
+
+  getImageUrl(imageUrl: string) {
+    return `url(${imageUrl})`;
+  }
 }
